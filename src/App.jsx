aaeb5b2cfd0a7fdad6c9a401f51d32ad27cf52ea
jenni1505 <jenni.korpi@seamk.fi>
2024-09-09 12:36:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Hyvää</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Perus</button>
+      <button onClick={() => setBad(bad + 1)}>Pahaa</button>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+export default App
