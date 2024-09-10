655f9e0425ee23bbaad41457d9de076cd23014d4
jenni1505 <jenni.korpi@seamk.fi>
2024-09-10 10:37:22 +0300

    Refactor: paositiiviseksi arvoksi lasketaan hyvä ja neutraali

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,29 @@ const App = () => {
 
   const total = good + neutral + bad
   const average = total === 0 ? 0 : (good - bad) / total
-  const positivePercentage = total === 0 ? 0 : (good / total) * 100
+  const positivePercentage = total === 0 ? 0 : ((good + neutral) / total) * 100
 
   return (
-    <div>
-      <h1>Anna Palaute</h1>
-      <Button onClick={() => setGood(good + 1)} text="Erinomainen" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="Perus" />
-      <Button onClick={() => setBad(bad + 1)} text="Pahaa" />
-      {total > 0 ? (
-        <Statistics 
-          good={good} 
-          neutral={neutral} 
-          bad={bad} 
-          total={total} 
-          average={average} 
-          positivePercentage={positivePercentage} 
-        />
-      ) : (
-        <p>No feedback given</p>
-      )}
-    </div>
+	<div>
+	  <h1>Anna palautetta</h1>
+	  <Button onClick={() => setGood(good + 1)} text="Hyvä" />
+	  <Button onClick={() => setNeutral(neutral + 1)} text="Neutraali" />
+	  <Button onClick={() => setBad(bad + 1)} text="Huono" />
+	  {total > 0 ? (
+		<Statistics 
+		  good={good} 
+		  neutral={neutral} 
+		  bad={bad} 
+		  total={total} 
+		  average={average} 
+		  positivePercentage={positivePercentage} 
+		/>
+	  ) : (
+		<p>Ei annettuja palautteita</p>
+	  )}
+	</div>
   )
 }
 
 export default App
+
